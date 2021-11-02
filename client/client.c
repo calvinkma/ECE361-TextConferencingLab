3ffa76bdaea2700546df0fe9c4216238da111f34
Calvin Ma <calvinkma@outlook.com>
2021-11-02 16:32:10 -0400

    Add client code to handle command line input.

--- client/client.c
+++ client/client.c
@@ -1,10 +1,110 @@
 // Standard Libraries
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <regex.h>
+#include <arpa/inet.h>
+#include <inttypes.h> // To print uint32_t, uint16_t nicely
 
 // Project Files
 #include "message.h"
 #include "constants.h"
+#include "commands.h"
+
+// Macrons
+#define PORT_NUMBER_LENGTH 2
+#define IPV4_BYTE_LENGTH 4
+#define IPV4_STR_LENGTH 15
+#define PORT_NUMBER_STR_LENGTH 5
+#define MAX_COMMAND_LENGTH MAX_NAME + MAX_DATA + PORT_NUMBER_LENGTH + IPV4_BYTE_LENGTH
+
+uint32_t get_server_addr_from_login_command(char* login_command) {
+    char server_addr_str[IPV4_STR_LENGTH];
+    uint32_t server_addr_binary;
+    sscanf(login_command, "%*s %*s %*s %s", server_addr_str);
+    short is_server_ip_valid = inet_pton(AF_INET, server_addr_str, &server_addr_binary);
+    if (is_server_ip_valid != 1) {
+        printf("Error: Invalid server address.\n");
+        exit(EXIT_FAILURE);
+    }
+    return server_addr_binary;
+}
+
+uint16_t get_server_port_number_from_login_command(char* login_command) {
+    char server_port_number_str[PORT_NUMBER_STR_LENGTH];
+    uint16_t server_port_number;
+    sscanf(login_command, "%*s %*s %*s %*s %s", server_port_number_str);
+    return htons((unsigned short)(atoi(server_port_number_str)));
+}
+
+Message* build_message_from_input(char* input_buf) {
+    unsigned int type;
+    unsigned int size;
+    unsigned char source[MAX_NAME];
+    unsigned char data[MAX_DATA];
+
+    if (is_string_of_pattern(input_buf, LOGIN_COMMAND_PATTERN)) {
+        type = LOGIN;
+        printf("IS LOGIN %d\n", type);
+    } else if (is_string_of_pattern(input_buf, LOGOUT_COMMAND_PATTERN)) {
+        printf("IS LOGOUT\n");
+    } else if (is_string_of_pattern(input_buf, JOIN_SESS_COMMAND_PATTERN)) {
+        printf("IS JOIN SESS\n");
+    } else if (is_string_of_pattern(input_buf, LEAVE_SESS_COMMAND_PATTERN)) {
+        printf("IS LEAVE SESS\n");
+    } else if (is_string_of_pattern(input_buf, CREATE_SESS_COMMAND_PATTERN)) {
+        printf("IS CREATE SESS\n");
+    } else if (is_string_of_pattern(input_buf, LIST_COMMAND_PATTERN)) {
+        printf("IS LIST\n");
+    } else if (is_string_of_pattern(input_buf, QUIT_COMMAND_PATTERN)) {
+        printf("IS QUIT\n");
+        exit(0);
+    } else {
+        printf("IS MESSAGE\n");
+    }
+
+    // TODO: Create the message
+    return NULL;
+}
 
 int main(void) {
-    printf("Hello world client %d\n", ASDF);
+    printf("Hello world client %d\n", MAX_COMMAND_LENGTH);
+
+    char input_buf[MAX_COMMAND_LENGTH];
+    Message* p_message;
+
+    // State Information
+    bool is_connection_setup = false;
+
+    // Server Information
+    short is_server_ip_valid;
+    uint32_t server_addr_binary;
+    uint16_t server_port_number;
+    struct sockaddr_in server_addr;
+
+    while (1) {
+        fgets(input_buf, sizeof(input_buf), stdin);
+
+        if (!is_connection_setup) {
+            if (!is_string_of_pattern(input_buf, LOGIN_COMMAND_PATTERN)) {
+                printf("You first need to log in to connect to the server.\n");
+                continue;
+            } else {
+                server_addr_binary = get_server_addr_from_login_command(input_buf);
+                server_port_number = get_server_port_number_from_login_command(input_buf);
+                // printf("%" PRIu32 "\n", server_addr_binary);
+                // printf("%" PRIu16 "\n", server_port_number);
+                // TODO: establish TCP connection
+                //   If successful, set is_connection_setup to true, create new listening thread
+                is_connection_setup = true;
+                //   If not, just continue in the loop
+            }
+        }
+
+        p_message = build_message_from_input(input_buf);
+        // Send message to server over the TCP connection
+    }
 }
+
+
+
---
+++ client/commands.h
@@ -0,0 +1,17 @@
+static char LOGIN_COMMAND_PATTERN[] = "^/login[[:space:]][[:alnum:]|_]+[[:space:]][[:alnum:]|_|[:punct:]]+[[:space:]]+[[:digit:]|.]+[[:space:]]+[[:digit:]]+[[:space:]]*$";
+static char LOGOUT_COMMAND_PATTERN[] = "^/logout[[:space:]]*$";
+static char JOIN_SESS_COMMAND_PATTERN[] = "^/joinsession[[:space:]][[:alnum:]|_]+[[:space:]]*$";
+static char LEAVE_SESS_COMMAND_PATTERN[] = "^/leavesession[[:space:]]*$";
+static char CREATE_SESS_COMMAND_PATTERN[] = "^/createsession[[:space:]][[:alnum:]|_]+[[:space:]]*$";
+static char LIST_COMMAND_PATTERN[] = "^/list[[:space:]]*$";
+static char QUIT_COMMAND_PATTERN[] = "^/quit[[:space:]]*$";
+
+bool is_string_of_pattern(char* input_buf, char* command_regex_pattern) {
+    regex_t regex;
+    int r = regcomp(&regex, command_regex_pattern, REG_EXTENDED);
+    if (r != 0) {
+        printf("When checking %s against %s, the regex does not compile.\n", input_buf, command_regex_pattern);
+        exit(EXIT_FAILURE);
+    }
+    return regexec(&regex, input_buf, 0, NULL, 0) == 0;
+}
